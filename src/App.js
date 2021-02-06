d9873434352eee67e4b812939bd4bf83140f5c5c
Thai Do <thai.hl.do@gmail.com>
2021-02-05 17:50:45 -0800

    Created signup component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import './App.css'
 import React from 'react'
+import Signup from './components/Signup'
 
 function App() {
   return (
     <div className="App">
       <h1></h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
