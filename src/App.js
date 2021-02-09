43b5032c1332703aa7d1b28a50ae00970fcfbbfc
Thai Do <thai.hl.do@gmail.com>
2021-02-08 16:34:13 -0800

    Created Login component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Container } from 'react-bootstrap'
 import { AuthProvider } from './context/AuthContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Dashboard from './components/Dashboard'
+import Login from './components/Login'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
             <Switch>
               <Route exact path="/" component={Dashboard} />
               <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
             </Switch>
           </AuthProvider>
         </Router>
