d2ba1747f9f9f94d2bd59ee4808c548069e88539
Thai Do <thai.hl.do@gmail.com>
2021-02-09 17:46:04 -0800

    Created todo list component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Form from './Form'
+import TodoList from './TodoList'
+import '../index.css'
 
 export default function TodoForm() {
+  const [inputText, setInputText] = useState('')
+
   return (
-    <div className="w-100 text-center mt-2">
-      <Link to="/">Cancel</Link>
+    <div className="App">
+      <header>
+        <h1>Thai's Todo List</h1>
+      </header>
+      <Form />
+      <TodoList />
     </div>
   )
 }
+
+// <div className="w-100 text-center mt-2">
+//   <Link to="/">Cancel</Link>
+// </div>
