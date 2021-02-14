f3410082e283f300ac91714cd36e46cdcd30a334
Thai Do <thai.hl.do@gmail.com>
2021-02-13 18:19:41 -0800

    Completed filter todos function

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Form from './Form'
 import TodoList from './TodoList'
@@ -7,6 +7,25 @@ import TodoList from './TodoList'
 export default function TodoForm() {
   const [inputText, setInputText] = useState('')
   const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  useEffect(() => {
+    filterHandler()
+  }, [todos, status])
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true))
+        break
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false))
+        break
+      default:
+        setFilteredTodos(todos)
+    }
+  }
 
   return (
     <div className="todo-form">
@@ -18,8 +37,13 @@ export default function TodoForm() {
         setInputText={setInputText}
         todos={todos}
         setTodos={setTodos}
+        setStatus={setStatus}
       />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   )
 }
